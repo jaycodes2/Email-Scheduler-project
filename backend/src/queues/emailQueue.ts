355d3b4826fe3172aa34248dbe0814c33cab0a97
Jayanthan <jayanthan1944@gmail.com>
2026-08-22 03:53:02 +0530

    Fixed Redis Queue

--- backend/src/queues/emailQueue.ts
+++ backend/src/queues/emailQueue.ts
@@ -1,8 +1,6 @@
 import { Queue } from "bullmq";
+import { redis } from "../config/redis.js";
 
-export const emailQueue=new Queue("email-queue",{
-    connection: {
-        host: "127.0.0.1",
-        port: 6379
-    }
+export const emailQueue = new Queue("email-queue", {
+  connection: redis,
 });
